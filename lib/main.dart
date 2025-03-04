6788e467a3ef2d562e1b31c8d955ffdc1c59e0b5
nn <townhouse.owner@gmail.com>
2025-03-05 06:30:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+//Developer: Nova Novriansyah Msc, MBA
+//Date: Mar-2025
+
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart'; // Ensure this import points to your actual SplashScreen file
+import 'package:firebase_core/firebase_core.dart'; //firebase
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DummyCommerce',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
